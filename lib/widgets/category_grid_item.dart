c678a6f94999051c851511d11e06ce62d3c55d95
manav <toremanav@gmail.com>
2023-05-11 19:48:00 +0530

    Inkwell feature added

--- lib/widgets/category_grid_item.dart
+++ lib/widgets/category_grid_item.dart
@@ -11,22 +11,28 @@ class CategoryGridItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        padding: const EdgeInsets.all(18),
-        decoration: BoxDecoration(
-            gradient: LinearGradient(
-          colors: [
-            category.color.withOpacity(0.55),
-            category.color.withOpacity(0.9),
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        )),
-        child: Text(
-          category.title,
-          style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                color: Theme.of(context).colorScheme.onBackground,
-              ),
-        ));
+    return InkWell(
+      onTap: () {},
+      splashColor: Colors.white,
+      borderRadius: BorderRadius.circular(16),
+      child: Container(
+          padding: const EdgeInsets.all(18),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(16),
+              gradient: LinearGradient(
+                colors: [
+                  category.color.withOpacity(0.55),
+                  category.color.withOpacity(0.9),
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              )),
+          child: Text(
+            category.title,
+            style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+          )),
+    );
   }
 }
