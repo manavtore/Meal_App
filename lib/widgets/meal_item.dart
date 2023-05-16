a7c105b7581f600e704bc79697cec698d57856cf
manav <toremanav@gmail.com>
2023-05-17 00:58:36 +0530

    updating  some UI

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -10,15 +10,26 @@ class MealItem extends StatelessWidget {
 
   final Meal meal;
 
+  @override
   Widget build(BuildContext context) {
     return Card(
+      margin: const EdgeInsets.all(8),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
+      ),
+      clipBehavior: Clip.hardEdge,
+      elevation: 2,
       child: InkWell(
         onTap: () {},
         child: Stack(
           children: [
             FadeInImage(
-                placeholder: MemoryImage(kTransparentImage),
-                image: NetworkImage(meal.imageUrl)),
+              placeholder: MemoryImage(kTransparentImage),
+              image: NetworkImage(meal.imageUrl),
+              fit: BoxFit.cover,
+              height: 200,
+              width: double.infinity,
+            ),
             Positioned(
               bottom: 0,
               left: 0,
