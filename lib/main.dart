5e94f85b7cf334ea3f13478cd17654ddc6236461
manav <toremanav@gmail.com>
2023-05-21 02:29:57 +0530

    adding bottomnavigatiobar

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import 'package:meals/screens/categories.dart';
+import 'package:meals/screens/tabs.dart';
 
 final theme = ThemeData(
   useMaterial3: true,
@@ -25,7 +26,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: theme,
-      home: const CategoriesScreen(),
+      home: const TabsScreen(),
     );
   }
 }
---
+++ lib/screens/tabs.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:meals/screens/categories.dart';
+import 'package:meals/screens/meals.dart';
+
+class TabsScreen extends StatefulWidget {
+  const TabsScreen({super.key});
+
+  @override
+  State<TabsScreen> createState() {
+    return _TabsScreen();
+  }
+}
+
+class _TabsScreen extends State<TabsScreen> {
+  int _selectedPageIndex = 0;
+
+  void _selectpage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget activePage = const CategoriesScreen();
+    var activePagetitle = 'Your Favourites';
+
+    if (_selectedPageIndex == 1) {
+      activePage = MealsScreen(meals: []);
+      activePagetitle = 'your Favourite';
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(activePagetitle),
+      ),
+      body: activePage,
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectpage,
+        currentIndex: _selectedPageIndex,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.set_meal),
+            label: 'Categories',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            label: 'Favourite',
+          )
+        ],
+      ),
+    );
+  }
+}
